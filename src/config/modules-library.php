a01f8cb6a158b8f7b4b252bc68b46d167132890a
Ifeoluwa Olawoye <dev@hostvillenigeria.org>
2019-06-30 19:45:16 +0100

    added 6 smetoolkit videos

--- src/config/modules-library.php
+++ src/config/modules-library.php
@@ -29,6 +29,84 @@ return [
                 'resource_video' => 'https://www.youtube.com/embed/nFdV4YsuNFQ',
                 'resource_title' => 'Pregnancy Tips - 1st Trimester',
                 'resource_description' => 'Learn more at http://google.com'
+            ],
+            [
+                'partner_id' => '0',
+                'resource_uuid' => '',
+                'resource_category' => '0',
+                'resource_subcategory' => '0',
+                'resource_type' => 'video',
+                'resource_subtype' => 'youtube',
+                'resource_thumb' => 'https://img.youtube.com/vi/rFmzLNtHIfc/sddefault.jpg',
+                'resource_image' => 'https://img.youtube.com/vi/rFmzLNtHIfc/default.jpg',
+                'resource_video' => 'https://www.youtube.com/embed/rFmzLNtHIfc',
+                'resource_title' => 'Business Plans (SMEToolkit Learn)',
+                'resource_description' => 'Get more videos at https://smetoolkit.ng/yes'
+            ],
+            [
+                'partner_id' => '0',
+                'resource_uuid' => '',
+                'resource_category' => '0',
+                'resource_subcategory' => '0',
+                'resource_type' => 'video',
+                'resource_subtype' => 'youtube',
+                'resource_thumb' => 'https://img.youtube.com/vi/aNs1bkvpbb4/sddefault.jpg',
+                'resource_image' => 'https://img.youtube.com/vi/aNs1bkvpbb4/default.jpg',
+                'resource_video' => 'https://www.youtube.com/embed/aNs1bkvpbb4',
+                'resource_title' => 'Customers - The Reason (SMEToolkit Learn)',
+                'resource_description' => 'Get more videos at https://smetoolkit.ng/yes'
+            ],
+            [
+                'partner_id' => '0',
+                'resource_uuid' => '',
+                'resource_category' => '0',
+                'resource_subcategory' => '0',
+                'resource_type' => 'video',
+                'resource_subtype' => 'youtube',
+                'resource_thumb' => 'https://img.youtube.com/vi/-Rel_aQhxV0/sddefault.jpg',
+                'resource_image' => 'https://img.youtube.com/vi/-Rel_aQhxV0/default.jpg',
+                'resource_video' => 'https://www.youtube.com/embed/-Rel_aQhxV0',
+                'resource_title' => 'Legal and Regulatory Issues (SMEToolkit Learn)',
+                'resource_description' => 'Get more videos at https://smetoolkit.ng/yes'
+            ],
+            [
+                'partner_id' => '0',
+                'resource_uuid' => '',
+                'resource_category' => '0',
+                'resource_subcategory' => '0',
+                'resource_type' => 'video',
+                'resource_subtype' => 'youtube',
+                'resource_thumb' => 'https://img.youtube.com/vi/nFdV4YsuNFQ/sddefault.jpg',
+                'resource_image' => 'https://img.youtube.com/vi/nFdV4YsuNFQ/default.jpg',
+                'resource_video' => 'https://www.youtube.com/embed/nFdV4YsuNFQ',
+                'resource_title' => ' (SMEToolkit Learn)',
+                'resource_description' => 'Get more videos at https://smetoolkit.ng/yes'
+            ],
+            [
+                'partner_id' => '0',
+                'resource_uuid' => '',
+                'resource_category' => '0',
+                'resource_subcategory' => '0',
+                'resource_type' => 'video',
+                'resource_subtype' => 'youtube',
+                'resource_thumb' => 'https://img.youtube.com/vi/c_vyU6hQbsU/sddefault.jpg',
+                'resource_image' => 'https://img.youtube.com/vi/c_vyU6hQbsU/default.jpg',
+                'resource_video' => 'https://www.youtube.com/embed/c_vyU6hQbsU',
+                'resource_title' => 'Value Proposition (SMEToolkit Learn)',
+                'resource_description' => 'Get more videos at https://smetoolkit.ng/yes'
+            ],
+            [
+                'partner_id' => '0',
+                'resource_uuid' => '',
+                'resource_category' => '0',
+                'resource_subcategory' => '0',
+                'resource_type' => 'video',
+                'resource_subtype' => 'youtube',
+                'resource_thumb' => 'https://img.youtube.com/vi/biJRRq5-zS8/sddefault.jpg',
+                'resource_image' => 'https://img.youtube.com/vi/biJRRq5-zS8/default.jpg',
+                'resource_video' => 'https://www.youtube.com/embed/biJRRq5-zS8',
+                'resource_title' => 'Operations - A Case Study (SMEToolkit Learn)',
+                'resource_description' => 'Get more videos at https://smetoolkit.ng/yes'
             ]
         ]
     ]
