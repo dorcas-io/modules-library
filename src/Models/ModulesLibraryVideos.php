2f8e33dd376b744dc680a0a27d68883ff979b5e0
Ifeoluwa Olawoye <dev@hostvillenigeria.org>
2020-03-22 12:06:54 +0100

    model connection fix

--- src/Models/ModulesLibraryVideos.php
+++ src/Models/ModulesLibraryVideos.php
@@ -7,9 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class ModulesLibraryVideos extends Model
 {
 	protected $table = 'mda_videos';
+	protected $connection = 'dorcas-api';
 
     protected $fillable = [
-        'name',
+        'partner_id',
+        'resource_uuid',
+        'resource_category',
+        'resource_subcategory',
+        'resource_type',
+        'resource_subtype',
+        'resource_thumb',
+        'resource_source',
+        'resource_title',
+        'resource_description'
     ];
 
 
